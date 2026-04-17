786d53071a28e8b66147a3907cbe8406ca1edce9
MrEinsteinE <einsteinellandala@gmail.com>
2026-04-17 23:45:49 +0530

    Clicks changed

--- src/test/java/com/sdet/pages/CheckoutPage.java
+++ src/test/java/com/sdet/pages/CheckoutPage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.sdet.utils.WaitUtils;
-
 /**
  * CheckoutPage — covers the full checkout flow:
  *   Step 1: Customer info  (/checkout-step-one.html)
